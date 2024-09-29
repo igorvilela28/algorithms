077d8e1e41341414ee38e323abb4deff10d5cc30
Igor Vilela <igorvilela28@hotmail.com>
2024-09-29 12:48:32 -0300

    Init project

---
+++ src/main/kotlin/knuth.kt
@@ -0,0 +1,28 @@
+import kotlin.random.Random
+
+/**
+ * Write a program that reads a sequence of words from standard input and prints one of those words uniformly at random.
+ * Do not store the words in an array or list. Instead, use Knuth’s method: when reading the ith word, select it with
+ * probability 1/i to be the champion, replacing the previous champion. After reading all of the words, print the
+ * surviving champion.
+ */
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        println("no args passed")
+        return
+    }
+
+    if (args.size == 1) {
+        println(args[0])
+        return
+    }
+
+    var reservoir = args[0]
+    for (i in 1..args.size) {
+        reservoir = args[randomNumber(i)]
+    }
+
+    println(reservoir)
+}
+
+private fun randomNumber(i: Int) = Random.nextInt(0, i)
