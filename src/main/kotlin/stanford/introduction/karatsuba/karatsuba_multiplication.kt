0ade94cc7f8e2f2048be01a6d56bfed101b371f5
Igor Vilela <igorvilela28@hotmail.com>
2025-01-27 18:06:41 -0300

    Add Karatsuba multiplication algorithm

---
+++ src/main/kotlin/stanford/introduction/karatsuba/karatsuba_multiplication.kt
@@ -0,0 +1,91 @@
+package stanford.introduction.karatsuba
+
+import java.math.BigInteger
+
+fun main() {
+    println("Hello World")
+
+    val result = multiplyByKaratsuba(BigInteger.valueOf(1234), BigInteger.valueOf(5678))
+    println(result)
+}
+
+internal fun multiplyByKaratsuba(value1: BigInteger, value2: BigInteger): BigInteger {
+
+    val s1 = value1.toString()
+    val s2 = value2.toString()
+
+    if (s1.length == 1 || s2.length == 1) {
+        return value1.multiply(value2)
+    }
+
+    val uniformizedNumbers = makeNumbersSameLength(s1, s2)
+
+    val a = uniformizedNumbers.first.splitInHalf().first.toBigInteger()
+    val b = uniformizedNumbers.first.splitInHalf().second.toBigInteger()
+    val c = uniformizedNumbers.second.splitInHalf().first.toBigInteger()
+    val d = uniformizedNumbers.second.splitInHalf().second.toBigInteger()
+
+    val n = uniformizedNumbers.first.length
+
+    println("A: $a - B: $b - C: $c - D: $d")
+
+    val p = a + b
+    val q = c + d
+
+    val ac = multiplyByKaratsuba(a, c)
+    val bd = multiplyByKaratsuba(b, d)
+    val pq = multiplyByKaratsuba(p, q)
+    val adbc = pq - ac - bd
+
+    val tenPowerN = BigInteger.TEN.pow(n)
+    val tenPowerHalfN = BigInteger.TEN.pow(n/2)
+
+    val result = tenPowerN.multiply(ac) + tenPowerHalfN.multiply(adbc) + bd
+    println("result: $value1 * $value2 = $result")
+    return result
+}
+
+internal fun String.splitInHalf(): Pair<String, String> {
+
+    val x = substring(0, length / 2)
+    val y = substring(length / 2, length)
+
+    return x to y
+}
+
+internal fun makeNumbersSameLength(s1: String, s2: String): Pair<String, String> {
+
+    var number1 = s1.makeStringPowerOfTwo()
+    var number2 = s2.makeStringPowerOfTwo()
+
+    if (number1.length == number2.length) {
+        return number1 to number2
+    }
+
+    if (number1.length < number2.length) {
+        while (number1.length < number2.length) {
+            number1 = "0" + number1
+        }
+        return number1 to number2
+    }
+
+    while (number2.length < number1.length) {
+        number2 = "0" + number2
+    }
+    return number1 to number2
+}
+
+internal fun String.makeStringPowerOfTwo(): String {
+
+    var s1 = this
+
+    while (!isPowerOfTwo(s1.length)) {
+        s1 = "0" + s1
+    }
+
+    return s1
+}
+
+internal fun isPowerOfTwo(number: Int): Boolean {
+    return number != 0 && (number and (number - 1) == 0)
+}
---
+++ src/test/kotlin/stanford/introduction/karatsuba/Karatsuba_multiplicationKtTest.kt
@@ -0,0 +1,88 @@
+package stanford.introduction.karatsuba
+
+import java.math.BigInteger
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class Karatsuba_multiplicationKtTest {
+
+
+    @Test
+    fun `should check if is power of 2`() {
+        assertFalse { isPowerOfTwo(0) }
+        assertFalse { isPowerOfTwo(3) }
+        assertFalse { isPowerOfTwo(12) }
+        assertFalse { isPowerOfTwo(15) }
+        assertFalse { isPowerOfTwo(23) }
+
+
+        assertTrue { isPowerOfTwo(2) }
+        assertTrue { isPowerOfTwo(4) }
+        assertTrue { isPowerOfTwo(8) }
+        assertTrue { isPowerOfTwo(16) }
+        assertTrue { isPowerOfTwo(32) }
+        assertTrue { isPowerOfTwo(64) }
+        assertTrue { isPowerOfTwo(128) }
+        assertTrue { isPowerOfTwo(256) }
+    }
+
+    @Test
+    fun `should append leading 0 when number length is not power of 2`() {
+        assertEquals("0123", "123".makeStringPowerOfTwo())
+        assertEquals("00123456", "123456".makeStringPowerOfTwo())
+        assertEquals("00012345", "12345".makeStringPowerOfTwo())
+        assertEquals("12345678", "12345678".makeStringPowerOfTwo())
+    }
+
+    @Test
+    fun `should transform numbers to same length`() {
+        assertEquals("12345678" to "00001234", makeNumbersSameLength("12345678", "00001234"))
+        assertEquals("00012345" to "00000001", makeNumbersSameLength("12345", "1"))
+        assertEquals("0123" to "0012", makeNumbersSameLength("123", "12"))
+        assertEquals("0001" to "0123", makeNumbersSameLength("1", "123"))
+    }
+    @Test
+    fun `should multiply 1234 times 5678`() {
+        val result = multiplyByKaratsuba(BigInteger.valueOf(1234), BigInteger.valueOf(5678))
+        assertEquals(BigInteger.valueOf(7006652), result)
+    }
+
+    @Test
+    fun `should multiply odd numbers - test case 01`() {
+        val bigNumber1 = BigInteger("46")
+        val bigNumber2 = BigInteger("102")
+        val expected = BigInteger("4692")
+        val result = multiplyByKaratsuba(bigNumber1, bigNumber2)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `should multiply odd numbers - test case 02`() {
+        val bigNumber1 = BigInteger("229")
+        val bigNumber2 = BigInteger("3826")
+        val expected = BigInteger("876154")
+        val result = multiplyByKaratsuba(bigNumber1, bigNumber2)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `should multiply 16 digits numbers`() {
+        val bigNumber1 = BigInteger("12345678")
+        val bigNumber2 = BigInteger("90123456")
+        val expected = BigInteger("1112635168023168")
+        val result = multiplyByKaratsuba(bigNumber1, bigNumber2)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `should multiply big numbers - solve 1st assignment`() {
+        val bigNumber1 = BigInteger("3141592653589793238462643383279502884197169399375105820974944592")
+        val bigNumber2 = BigInteger("2718281828459045235360287471352662497757247093699959574966967627")
+        val expected = BigInteger("8539734222673567065463550869546574495034888535765114961879601127067743044893204848617875072216249073013374895871952806582723184")
+        val result = multiplyByKaratsuba(bigNumber1, bigNumber2)
+        assertEquals(expected, result)
+    }
+
+}
