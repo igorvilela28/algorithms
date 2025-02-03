36da5e2dac9c0de1a618e2e9bef1112789468650
Igor Vilela <igorvilela28@hotmail.com>
2025-02-03 15:36:19 -0300

    Add algorithm to count number of  array inversions

--- src/main/kotlin/stanford/introduction/counting_inversions/counting_inversions.kt
+++ src/main/kotlin/stanford/introduction/counting_inversions/counting_inversions.kt
@@ -1,11 +1,61 @@
 package stanford.introduction.counting_inversions
 
-internal fun counting_inversions(a: List<Int>): Int {
+/**
+ * Sorts A and return it and the number of inversions
+ */
+internal fun countInversions(a: List<Int>): Pair<List<Int>, Int> {
 
     // base case
     if (a.size <= 1) {
-        return 0
+        return a to 0
     }
 
-    return 0
+    if (a.size == 2) {
+        val isInverted = a[0] > a[1]
+        return if (isInverted) a.reversed() to 1 else a to 0
+    }
+
+    val splitted = split(a)
+    val leftSide = splitted.first
+    val rightSide = splitted.second
+
+    val leftInversions = countInversions(leftSide)
+    val rightInversions = countInversions(rightSide)
+    val splittedInversions = mergeAndCountSpplited(leftSide, rightSide)
+    return splittedInversions.first to (leftInversions.second + rightInversions.second + splittedInversions.second)
+}
+
+/**
+ * merges 2 sorted arrays into one and counts the number of spplited inversions
+ */
+internal fun mergeAndCountSpplited(sortedLeftSide: List<Int>, sortedRightSide: List<Int>): Pair<List<Int>, Int> {
+    var i = 0
+    var j = 0
+    val n = sortedLeftSide.size + sortedRightSide.size
+    var count = 0
+    val merged = mutableListOf<Int>()
+    for (k in 0 until n) {
+
+        if (i == sortedLeftSide.size) {
+            merged.add(sortedRightSide[j])
+            j++
+        } else if (j == sortedRightSide.size) {
+            merged.add(sortedLeftSide[i])
+            i++
+        } else if (sortedLeftSide[i] < sortedRightSide[j]) {
+            merged.add(sortedLeftSide[i])
+            i++
+        } else {
+            merged.add(sortedRightSide[j])
+            j++
+            count += sortedLeftSide.size - i
+        }
+    }
+    return merged to count
+}
+
+internal fun split(a: List<Int>): Pair<List<Int>, List<Int>> {
+    val size = if (a.size % 2 == 0) a.size / 2 else (a.size + 1) / 2
+    val splitted = a.chunked(size)
+    return splitted.first() to splitted.last()
 }
--- src/test/kotlin/stanford/introduction/counting_inversions/CountingInversionsTest.kt
+++ src/test/kotlin/stanford/introduction/counting_inversions/CountingInversionsTest.kt
@@ -6,9 +6,52 @@ import kotlin.test.assertEquals
 class CountingInversionsTest {
 
     @Test
+    fun `should split even list of numbers in half`() {
+        val a = listOf(1, 3, 5, 2, 4, 6)
+        val expected = listOf(1,3,5) to listOf(2,4,6)
+        val result = split(a)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `should split odd list of numbers in half`() {
+        val a = listOf(1, 3, 5, 2, 4)
+        val expected = listOf(1,3,5) to listOf(2,4)
+        val result = split(a)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `should merge and count splitted for even sorted arrays`() {
+        val a = listOf(1, 4)
+        val b = listOf(2,3)
+        val expected = listOf(1,2,3,4) to 2
+        val result = mergeAndCountSpplited(a, b)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `should merge and count splitted for odd sorted arrays`() {
+        val a = listOf(1, 4, 5)
+        val b = listOf(2,3)
+        val expected = listOf(1,2,3,4,5) to 4
+        val result = mergeAndCountSpplited(a, b)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `should merge and count splitted for odd sorted arrays - 02`() {
+        val a = listOf(1, 5)
+        val b = listOf(2,3,4)
+        val expected = listOf(1,2,3,4,5) to 3
+        val result = mergeAndCountSpplited(a, b)
+        assertEquals(expected, result)
+    }
+
+    @Test
     fun `should count test case 01`() {
         val a = listOf(1, 3, 5, 2, 4, 6)
-        val result = counting_inversions(a)
-        assertEquals(3, result)
+        val result = countInversions(a)
+        assertEquals(3, result.second)
     }
 }
