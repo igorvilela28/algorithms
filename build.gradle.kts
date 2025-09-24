acc0f1d07821ce0f1cb6d002b9d24c0aa5b411e7
Igor Vilela <igorvilela28@hotmail.com>
2025-09-24 19:42:00 -0300

    Setup jvm args for project and tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "2.2.0"
     application
 }
 
@@ -16,6 +16,10 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    maxHeapSize = "6g"
+    maxParallelForks = 1
+    jvmArgs("-XX:+UseG1GC", "-XX:MaxGCPauseMillis=200","-Xss4m")
+    jvmArgs()
 }
 
 kotlin {
